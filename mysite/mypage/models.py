d5ef4a1b7d0077ed3f5a39d1718c075a56551848
ArsenyKokotov <akokotov96@gmail.com>
2021-06-24 02:06:22 -0400

    added some image functionalities

--- mysite/mypage/models.py
+++ mysite/mypage/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Description(models.Model):
     txt_description = models.CharField(max_length=500)
+    #profile_pic = models.ImageField(upload_to="profile_pics")
+    profile_pic = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.txt_description
